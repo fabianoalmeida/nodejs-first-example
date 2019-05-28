628629fc560121fb1dfcad70a41d25338915dfdf
Fabiano Almeida <fabianoalmeida@gmail.com>
2019-05-27 21:35:57 -0300

    Organizing in modules

--- src/index.js
+++ src/index.js
@@ -3,76 +3,25 @@ import uuidv4 from 'uuid/v4';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import express from 'express';
+import models from './models';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-let users = {
-    1: {
-      id: '1',
-      username: 'Robin Wieruch',
-    },
-    2: {
-      id: '2',
-      username: 'Dave Davids',
-    },
-};
-  
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'By World',
-        userId: '2',
-    },
-};
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
-});
-
-app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
-});
-
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on user resource');
-});
-  
-app.put('/users/:userId', (req, res) => {
-    return res.send( `PUT HTTP method on user/${req.params.userId} resource`);
-});
-  
-app.delete('/users/:userId', (req, res) => {
-    return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-});
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
-});
-  
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-        id,
-        text: req.body.text,
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
     };
-  
-    messages[id] = message;
-  
-    return res.send(message);
+    next();
 });
+
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
     console.log(`Example app listening on port ${process.env.PORT}!`),
